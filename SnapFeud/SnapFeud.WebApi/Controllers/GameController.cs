9b575a94a06f64c4a7e371474adc7126e9b9efb8
Mats Sjövall <matssjovall@hotmail.com>
2016-04-02 12:50:17 -0700

    New API

--- SnapFeud/SnapFeud.WebApi/Controllers/GameController.cs
+++ SnapFeud/SnapFeud.WebApi/Controllers/GameController.cs
@@ -1,100 +1,37 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Mvc;
+using SnapFeud.WebApi.Models;
 
 namespace SnapFeud.WebApi.Controllers
 {
     [Route("api/[controller]/[action]")]
     public class GameController : Controller
     {
-        private static GameState currentGameState;
-
-        private static readonly Dictionary<string, UserState> users = new Dictionary<string, UserState>();
-
-        private static readonly string[] challenges = {"Laptop", "Phone"};
-
-        private static readonly Random random = new Random();
-
-        public GameController()
-        {
-            if (currentGameState == null)
-            {
-                currentGameState = new GameState {GameId = Guid.NewGuid()};
-                CreateNewChallenge(currentGameState);
-            }
-        }
-
         [HttpGet("{userName}")]
-        public Guid JoinGame(string userName)
+        public Game CreateGame(string userName)
         {
-            if (!users.ContainsKey(userName))
-            {
-                users[userName] = new UserState {UserName = userName};
-            }
-
-            users[userName].GameId = currentGameState.GameId;
-
-            return currentGameState.GameId;
+            return null;
         }
 
         [HttpGet]
-        public GameState GetGameState()
+        public Game GetGame(Guid gameId)
         {
-            if (DateTime.UtcNow > currentGameState.ExpiryTime)
-            {
-                CreateNewChallenge(currentGameState);
-            }
-
-            return currentGameState;
+            return null;
         }
 
         [HttpPost("{gameId}/{userName}")]
-        public async Task<bool> SubmitAnswer(string userName, Guid gameId)
+        public async Task<Game> SubmitAnswer(Guid gameId)
         {
+            if (!Request.ContentLength.HasValue)
+            {
+                return null;
+            }
+
             byte[] photo = new byte[Request.ContentLength.Value];
             await Request.Body.ReadAsync(photo, 0, photo.Length);
 
-            if (currentGameState == null || gameId != currentGameState.GameId || !users.ContainsKey(userName))
-            {
-                return false;
-            }
-
-            users[userName].Score += 1;
-
-            return true;
+            return null;
         }
-
-        [HttpGet("{gameId}")]
-        public IEnumerable<UserState> GetScores(Guid gameId)
-        {
-            return users.Values.Where(x => x.GameId == gameId);
-        }
-
-        private void CreateNewChallenge(GameState gameState)
-        {
-            var nextChallenge = random.Next(0, challenges.Length);
-            gameState.ExpiryTime = DateTime.UtcNow.AddMinutes(2);
-            gameState.CurrentChallenge = challenges[nextChallenge];
-        }
-    }
-
-    public class GameState
-    {
-        public string CurrentChallenge { get; set; }
-
-        public DateTime ExpiryTime { get; set; }
-
-        public Guid GameId { get; set; }
-    }
-
-    public class UserState
-    {
-        public string UserName { get; set; }
-
-        public int Score { get; set; }
-
-        public Guid GameId { get; set; }
     }
 }
